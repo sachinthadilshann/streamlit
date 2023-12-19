1603aa286cc983104edf340aecb1595b4118d4b2
Sachintha Dilshan <sachinthadilshann@gmail.com>
2023-12-19 19:44:02 +0530

    update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from datetime import time
-
+from datetime import time as dt_time
 import streamlit as st
 import pandas as pd
 import requests
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 st.title('Voltage Forecasting using ARIMA')
 st.write('This app uses ARIMA model to forecast the Voltage.')
@@ -25,7 +28,8 @@ def load_data():
         # Convert the nested data to a dataframe
         df = pd.DataFrame(list(nested_data.items()), columns=['Timestamp', 'Current'])
         df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-        df = df.dropna()  # Drop rows with NaT (Not a Time) values
+        df['Current'] = pd.to_numeric(df['Current'], errors='coerce')  # Convert 'Current' to numeric
+        df = df.dropna(subset=['Current'])  # Drop rows with NaN in 'Current'
         df.set_index('Timestamp', inplace=True)  # Set 'Timestamp' as the index
 
         return df
@@ -34,9 +38,13 @@ def load_data():
         return None
 
 # Add a sidebar with options
-st.sidebar.title("Options")
+
+
+image_url = "logo.png"
+st.sidebar.image(image_url)
 display_dataframe = st.sidebar.checkbox("Display Dataframe")
 plot_dataframe = st.sidebar.checkbox("Plot Dataframe in Real-time")
+run_forecast_button = st.sidebar.checkbox("Run ARIMA Forecast")
 
 # Load data
 df = load_data()
@@ -48,7 +56,7 @@ if display_dataframe and df is not None:
 
 # Plot dataframe in real-time with a rolling average if selected in the sidebar
 if plot_dataframe and df is not None:
-    st.subheader("Plotting Dataframe in Real-time with Rolling Average")
+    st.subheader("Plotting dataset")
 
     # Create a placeholder for the chart
     chart_placeholder = st.line_chart(df['Current'])  # Use 'Current' column directly as it is now the index
@@ -63,3 +71,57 @@ if plot_dataframe and df is not None:
 
             # Update the chart with the rolling average
             chart_placeholder.line_chart(rolling_avg)
+
+# Add ARIMA model fitting and forecasting section
+if run_forecast_button:
+    # Split the data into train and test sets
+    st.subheader('Train and Test Sets')
+    split = st.slider('Select the train-test split ratio', 0.5, 0.9, 0.8, 0.01)
+    train_size = int(len(df) * split)
+    train, test = df[0:train_size], df[train_size:len(df)]
+    st.write('Train set size:', len(train))
+    st.write('Test set size:', len(test))
+
+    # Fit the ARIMA model
+    st.subheader('ARIMA Model')
+    p = st.number_input('Enter the order of AR term', 0, 10, 5)
+    d = st.number_input('Enter the degree of differencing', 0, 10, 1)
+    q = st.number_input('Enter the order of MA term', 0, 10, 0)
+    model = sm.tsa.ARIMA(train['Current'], order=(p, d, q))
+    model_fit = model.fit()
+    st.write(model_fit.summary())
+
+    # Forecast for the next steps
+    st.subheader('Forecast')
+    steps = st.number_input('Enter the number of steps to forecast', 1, len(test), 10)
+    forecast = model_fit.get_forecast(steps=steps)
+    forecast_values = forecast.predicted_mean
+    st.write('Forecasted current for the next', steps, 'steps:')
+    st.write(forecast_values)
+
+    # Evaluation
+    mse = mean_squared_error(test['Current'][:steps], forecast_values)
+    rmse = np.sqrt(mse)
+    st.write('Mean Squared Error:', mse)
+    st.write('Root Mean Squared Error:', rmse)
+
+    # Plot forecast
+    st.subheader('Forecast Plot')
+    fig, ax = plt.subplots(figsize=(20, 8))
+    ax.plot(train['Current'], label='Train')
+    ax.plot(test['Current'], label='Test')
+    ax.plot(forecast_values, label='Forecast')
+    ax.set_xlabel('Timestamp')
+    ax.set_ylabel('Current')
+    ax.legend()
+    st.pyplot(fig)
+
+    # Future Predictions
+    st.subheader('Future Predictions')
+    future_steps = st.number_input('Enter the number of future steps to predict', 1, 1000, 10)
+    future_forecast_obj = model_fit.get_forecast(steps=future_steps)
+    future_forecast = future_forecast_obj.predicted_mean
+    st.write('Future forecast for the next', future_steps, 'steps:')
+    st.write(future_forecast)
+
+
