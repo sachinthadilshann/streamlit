1b8b8f5905c2fa0afdc9becc1ba8749fa359ba6b
Sachintha Dilshan <sachinthadilshann@gmail.com>
2023-12-19 18:23:19 +0530

    update

--- app.py
+++ app.py
@@ -1,91 +1,65 @@
-import time
+from datetime import time
+
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
-from sklearn.metrics import mean_squared_error
+import requests
 
 st.title('Voltage Forecasting using ARIMA')
-st.write('This app uses ARIMA model to forecast the Voltage .')
-data = st.file_uploader('Upload a CSV file(Data,Voltage) only ', type='csv')
+st.write('This app uses ARIMA model to forecast the Voltage.')
 
-if data is not None:
-    df = pd.read_csv(data, parse_dates=['Date'], index_col='Date')
-    st.write(df.head())
+# Replace the URL with the actual URL of your Firebase Realtime Database
+database_url = "https://solar-monitoring-system-7b380-default-rtdb.firebaseio.com/"
 
+def load_data():
+    response = requests.get(database_url + ".json")
 
-    st.subheader('Voltage Plot')
-    fig, ax = plt.subplots(figsize=(20, 6))
-    ax.plot(df['Voltage'], label='Voltage')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Voltage')
-    ax.legend()
-    st.pyplot(fig)
+    if response.status_code == 200:
+        data = response.json()
 
-    # Split the data into train and test sets
-    st.subheader('Train and Test Sets')
-    split = st.slider('Select the train-test split ratio', 0.5, 0.9, 0.8, 0.01)
-    train_size = int(len(df) * split)
-    train, test = df[0:train_size], df[train_size:len(df)]
-    st.write('Train set size:', len(train))
-    st.write('Test set size:', len(test))
+        solar_panel_id = '0001'
+        device_id = 'Current'
 
-    # Fit the ARIMA model
-    st.subheader('ARIMA Model')
-    p = st.number_input('Enter the order of AR term', 0, 10, 5)
-    d = st.number_input('Enter the degree of differencing', 0, 10, 1)
-    q = st.number_input('Enter the order of MA term', 0, 10, 0)
-    model = ARIMA(train, order=(p, d, q))
-    model_fit = model.fit()
-    st.write(model_fit.summary())
+        # Extract the relevant nested data
+        nested_data = data.get('solar_panel', {}).get(solar_panel_id, {}).get(device_id, {})
 
-    st.subheader('Forecast')
-    steps = st.number_input('Enter the number of steps to forecast', 1, len(test), 10)
-    forecast_obj = model_fit.get_forecast(steps, alpha=0.05)
-    forecast = forecast_obj.predicted_mean
-    se = forecast_obj.se_mean
-    conf = forecast_obj.conf_int()
-    st.write('Forecasted temperature for the next', steps, 'days:')
-    st.write(forecast)
+        # Convert the nested data to a dataframe
+        df = pd.DataFrame(list(nested_data.items()), columns=['Timestamp', 'Current'])
+        df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+        df = df.dropna()  # Drop rows with NaT (Not a Time) values
+        df.set_index('Timestamp', inplace=True)  # Set 'Timestamp' as the index
 
+        return df
+    else:
+        st.write("Failed to retrieve data from Firebase:", response.status_code)
+        return None
 
-    st.subheader('Evaluation')
-    mse = mean_squared_error(test['Voltage'][:steps], forecast)
-    rmse = np.sqrt(mse)
-    st.write('Mean Squared Error:', mse)
-    st.write('Root Mean Squared Error:', rmse)
+# Add a sidebar with options
+st.sidebar.title("Options")
+display_dataframe = st.sidebar.checkbox("Display Dataframe")
+plot_dataframe = st.sidebar.checkbox("Plot Dataframe in Real-time")
 
-    st.subheader('Forecast Plot')
-    fig, ax = plt.subplots(figsize=(20, 8))
-    ax.plot(train['Voltage'], label='Train')
-    ax.plot(test['Voltage'], label='Test')
-    ax.plot(forecast, label='Forecast')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Voltage')
-    ax.legend()
-    st.pyplot(fig)
+# Load data
+df = load_data()
 
-    st.subheader('Future Predictions')
-    future_steps = st.number_input('Enter the number of future steps to predict', 1, 1000, 10)
-    future_forecast_obj = model_fit.get_forecast(future_steps, alpha=0.05)
-    future_forecast = future_forecast_obj.predicted_mean
-    future_conf = future_forecast_obj.conf_int()
-    st.write('Future forecast for the next', future_steps, 'hours:')
-    st.write(future_forecast)
+# Display dataframe if selected in the sidebar
+if display_dataframe and df is not None:
+    st.subheader("Displaying Dataframe")
+    st.write(df)
 
-    st.subheader('Original Data Plot')
-    fig, ax = plt.subplots(figsize=(20, 6))
-    ax.plot(df['Voltage'], label='Voltage')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Voltage')
-    ax.legend()
-    st.pyplot(fig)
+# Plot dataframe in real-time with a rolling average if selected in the sidebar
+if plot_dataframe and df is not None:
+    st.subheader("Plotting Dataframe in Real-time with Rolling Average")
 
-    st.subheader('Forecast Plot')
-    fig, ax = plt.subplots(figsize=(20, 8))
-    ax.plot(future_forecast, label='Future Forecast')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Voltage')
-    ax.legend()
-    st.pyplot(fig)
+    # Create a placeholder for the chart
+    chart_placeholder = st.line_chart(df['Current'])  # Use 'Current' column directly as it is now the index
+
+    while True:
+        # Reload data in a loop
+        df = load_data()
+
+        if df is not None:
+            # Calculate a rolling average with a window size of 5
+            rolling_avg = df['Current'].rolling(window=5).mean()
+
+            # Update the chart with the rolling average
+            chart_placeholder.line_chart(rolling_avg)
