a525d6b09b6f6f75d6998e1eaeb6db7ff63a57e2
Sachintha Dilshan <sachinthadilshann@gmail.com>
2023-12-19 23:26:04 +0530

    update

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from datetime import datetime, timedelta  # Import timedelta
-from datetime import time as dt_time
+from datetime import datetime, timedelta
 import streamlit as st
 import pandas as pd
 import requests
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.arima.model import ARIMA
 import numpy as np
-from statsmodels.tsa.arima.model import ARIMA  # Import ARIMA from statsmodels
+from statsmodels.tools.sm_exceptions import ConvergenceWarning
 
-st.title('Voltage Forecasting using ARIMA')
-st.write('This app uses ARIMA model to forecast the Voltage.')
+
+st.title('Current Forecasting using ARIMA')
+st.write('This app uses ARIMA model to forecast the Current.')
 
 # Replace the URL with the actual URL of your Firebase Realtime Database
 database_url = "https://solar-monitoring-system-7b380-default-rtdb.firebaseio.com/"
@@ -22,16 +23,16 @@ def load_data():
         data = response.json()
 
         solar_panel_id = '0001'
-        device_id = 'Voltage'  # Assuming the voltage data is under the 'Voltage' key
+        device_id = 'Current'  # Assuming the current data is under the 'Current' key
 
         # Extract the relevant nested data
         nested_data = data.get('solar_panel', {}).get(solar_panel_id, {}).get(device_id, {})
 
         # Convert the nested data to a dataframe
-        df = pd.DataFrame(list(nested_data.items()), columns=['Timestamp', 'Voltage'])
+        df = pd.DataFrame(list(nested_data.items()), columns=['Timestamp', 'Current'])
         df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-        df['Voltage'] = pd.to_numeric(df['Voltage'], errors='coerce')  # Convert 'Voltage' to numeric
-        df = df.dropna(subset=['Voltage'])  # Drop rows with NaN in 'Voltage'
+        df['Current'] = pd.to_numeric(df['Current'], errors='coerce')  # Convert 'Current' to numeric
+        df = df.dropna(subset=['Current'])  # Drop rows with NaN in 'Current'
         df.set_index('Timestamp', inplace=True)  # Set 'Timestamp' as the index
 
         # Resample to 5-minute intervals
@@ -48,7 +49,6 @@ st.sidebar.image(image_url)
 display_dataframe = st.sidebar.checkbox("Display Dataframe")
 plot_dataframe = st.sidebar.checkbox("Plot Dataframe in Real-time")
 run_forecast_button = st.sidebar.checkbox("Run ARIMA Forecast")
-future_forecast_button = st.sidebar.checkbox("Future Forecast")
 
 # Load data outside the loop to avoid unnecessary data fetching
 df = load_data()
@@ -73,85 +73,66 @@ if plot_dataframe and df is not None:
 
         if df_filtered is not None:
             # Calculate a rolling average with a window size of 5
-            rolling_avg = df_filtered['Voltage'].rolling(window=5).mean()
+            rolling_avg = df_filtered['Current'].rolling(window=5).mean()
 
             # Clear the previous chart and update with the new data
             chart_placeholder.line_chart(rolling_avg, use_container_width=True)
 
-# Run ARIMA Forecast button logic
+    # ... (previous code)
+
+    # Run ARIMA Forecast button logic
 if run_forecast_button and df is not None:
-    # Split the data into train and test sets
-    st.subheader('Train and Test Sets')
-    split = st.slider('Select the train-test split ratio', 0.5, 0.9, 0.8, 0.01)
-    train_size = int(len(df) * split)
-    train, test = df.iloc[:train_size], df.iloc[train_size:]
 
-    # Fit the ARIMA model
-    st.subheader('ARIMA Model')
-    p = st.number_input('Enter the order of AR term', 0, 10, 5)
-    d = st.number_input('Enter the degree of differencing', 0, 10, 1)
-    q = st.number_input('Enter the order of MA term', 0, 10, 0)
+        X = df['Current'].values
 
-    # Create and fit the ARIMA model
-    model = ARIMA(train['Voltage'], order=(p, d, q))
-    model_fit = model.fit()
-    st.write(model_fit.summary())
+        # Split the data into training and testing sets (80% training, 20% testing)
+        size = int(len(X) * 0.8)
+        train, test = X[:size], X[size:]
 
-    # Forecast
-    st.subheader('Forecast')
-    steps = st.number_input('Enter the number of steps to forecast', 1, len(test), 10)
-    forecast_obj = model_fit.get_forecast(steps, alpha=0.05)
-    forecast = forecast_obj.predicted_mean
-    se = forecast_obj.se_mean
-    conf = forecast_obj.conf_int()
-    st.write('Forecasted voltage for the next', steps, 'steps:')
-    st.write(forecast)
+        # Initialize a list to store the training data
+        history = [x for x in train]
 
-    # Evaluation
-    st.subheader('Evaluation')
-    mse = mean_squared_error(test['Voltage'][:steps], forecast)
-    rmse = np.sqrt(mse)
-    st.write('Mean Squared Error:', mse)
-    st.write('Root Mean Squared Error:', rmse)
+        # Initialize a list to store the predicted values
+        predictions = []
 
-    # Forecast Plot
-    st.subheader('Forecast Plot')
-    fig, ax = plt.subplots(figsize=(20, 8))
-    ax.plot(train['Voltage'], label='Train')
-    ax.plot(test['Voltage'], label='Test')
-    ax.plot(forecast.index, forecast.values, label='Forecast')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Voltage')
-    ax.legend()
-    st.pyplot(fig)
+        # Loop through the testing set to make predictions
+        for t in range(len(test)):
+            # Try different orders
+            model = ARIMA(history, order=(5, 2, 1))
+            try:
+                model_fit = model.fit()
+            except ConvergenceWarning:
+                # Handle convergence warning, try different orders or methods
+                continue
 
-# Future Forecast button logic
-if future_forecast_button and df is not None:
-    st.subheader('Future Forecast')
+            output = model_fit.forecast()
+            yhat = output[0]
+            predictions.append(yhat)
+            obs = test[t]
+            history.append(obs)
+            #st.write(f'Predicted: {yhat:.2f}, Expected: {obs:.2f}')
 
-    # Get the latest timestamp in the existing data
-    latest_timestamp = df.index[-1]
+        # Evaluate the model
+        mse = mean_squared_error(test, predictions)
+        rmse = np.sqrt(mse)
+        st.subheader('Model Evaluation')
+        st.write('Mean Squared Error:', mse)
+        st.write('Root Mean Squared Error:', rmse)
 
-    # Define the number of future steps to forecast
-    future_steps = st.number_input('Enter the number of steps for future forecasting', 1, 100, 10)
+        # Plot the actual vs. predicted values
+        st.subheader('Actual vs. Predicted Plot')
+        plt.plot(test, label='Actual')
+        plt.plot(predictions, label='Predicted')
+        plt.legend()
+        st.pyplot(plt)
 
-    # Generate future timestamps based on the latest timestamp and the desired number of steps
-    future_timestamps = [latest_timestamp + timedelta(seconds=30 * i) for i in range(1, future_steps + 1)]
-
-    # Forecast future values using the trained model
-    future_forecast_obj = model_fit.get_forecast(steps=future_steps, alpha=0.05)
-    future_forecast = future_forecast_obj.predicted_mean
-
-    # Display the future forecast
-    st.write('Future forecast for the next', future_steps, 'steps:')
-    st.write(future_forecast)
-
-    # Plot the future forecast
-    st.subheader('Future Forecast Plot')
-    fig, ax = plt.subplots(figsize=(20, 8))
-    ax.plot(df['Voltage'], label='Historical Data')
-    ax.plot(future_timestamps, future_forecast.values, label='Future Forecast')
-    ax.set_xlabel('Date')
-    ax.set_ylabel('Voltage')
-    ax.legend()
-    st.pyplot(fig)
+        st.subheader('Forecast Plot')
+        # Forecast Plot
+        fig, ax = plt.subplots(figsize=(20, 8))
+        ax.plot(X, label='Historical Data')
+        ax.plot(np.arange(size, size + len(test)), test, label='Test')
+        ax.plot(np.arange(size, size + len(test)), predictions, label='Forecast')
+        ax.set_xlabel('Data Point')
+        ax.set_ylabel('Current')
+        ax.legend()
+        st.pyplot(fig)
